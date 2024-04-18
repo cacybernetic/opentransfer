2413b0f9381965079e3586748bf914a09985c7d2
Obrymec <obrymecsprinces@gmail.com>
2024-04-18 02:44:40 -0400

    Add build number.

--- src/common/i18n/english.js
+++ src/common/i18n/english.js
@@ -4,7 +4,7 @@
 * @author Obrymec - obrymecsprinces@gmail.com
 * @supported DESKTOP & MOBILE
 * @created 2024-03-04
-* @updated 2024-04-17
+* @updated 2024-04-18
 * @file english.js
 * @type {English}
 * @version 0.0.2
@@ -330,7 +330,7 @@ const english = {
     `,
     tr86: "Ours services",
     tr87: "Decline",
-    tr88: "Download Open Transfer | v0.5.0",
+    tr88: "Download Open Transfer | v0.5.0b15",
     tr89: `
       I've read and agree <a target = "_blank" class = "terms"
       href = "https://www.termsfeed.com/live/7320699f-80dd-4375-8d3f-22cc7a79b945"
